f6d741a807bebe33a09428911f8bca941d22e1cc
chstrcpprpt <cjhallen@gmail.com>
2020-04-04 09:05:42 +1100

    added production settings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-// const path = require("path");
+const path = require("path");
 
 // location of api routes
 const books = require("./routes/api/books");
@@ -28,15 +28,15 @@ mongoose.connect(db,
 // Use routes - api/books/* will go to this file
 app.use("/api/books", books);
 
-// // Serve static assets if in production
-// if(process.env.NODE_ENV === "production") {
-//   //set static folder
-//   app.use(express.static("client/build"));
+// Serve static assets if in production
+if(process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // START SERVER
 // Define port localhost:3001
